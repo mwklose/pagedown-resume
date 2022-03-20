0589994f9e31b7d00833bf85074b89de20debb58
Mark Klose <markklose17@gmail.com>
2022-03-20 15:27:43 -0400

    Gather Data half finished

--- gather_data.R
+++ gather_data.R
@@ -1,2 +1,10 @@
 raw_info <- read_csv(resume_file)
 
+sections <- c("blurb", "contact", "education", "skills", "research", "award", "service", "teaching", "paper", "service")
+
+info <- lapply(sections, function(x){
+  raw_info %>% 
+    dplyr::filter(Section == x)
+})
+
+names(info) <- sections
---
+++ make_sections.R
@@ -0,0 +1,20 @@
+
+make_contact <- function(infotable, priority=1){
+  print(infotable$Value)
+}
+
+make_blurb <- function(infotable, priority=1){
+  print(infotable$Value)
+}
+
+make_education <- function(infotable, priority=1){
+  print(infotable$Value)
+}
+
+make_research <- function(infotable, priority=1){
+  print(infotable$Value)
+}
+
+make_skills <- function(infotable, priority=1){
+  print(infotable$Value)
+}
